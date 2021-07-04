36f8b3efe4a79f7fa3709212f3949f7db10f539f
unknown <ponniahk78@gmail.com>
2021-07-04 17:13:35 +0530

    integrate json input data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,125 @@
 import React from "react";
+//import axios from "axios";
 import "./styles/loading.scss";
 import "./styles/cartPage.scss";
 import './styles/popupPage.scss';
 import Loader from './Loader';
 import Popup from './popupPage';
-import axios from "axios";
-const Api='/mocsyamtestapi-env.eba-dvawv2zg.ap-south-1.elasticbeanstalk.com/products/?pageNumber=0';
+import amd1 from './images/amd-processor/amd-1.jpg';
+import amd2 from './images/amd-processor/amd-2.jpg';
+import amd3 from './images/amd-processor/amd-3.jpg';
+import zeb1 from './images/zebronics/zeb-1.png';
+import zeb2 from './images/zebronics/zeb-2.jpg';
+import zeb3 from './images/zebronics/zeb-3.jpg';
+import hp1 from './images/HPPavilion/hp-1.jpg';
+import hp2 from './images/HPPavilion/hp-2.jpg';
+import hp3 from './images/HPPavilion/hp-3.jpg';
+import sony1 from './images/SonyPS/sony1.jpg';
+import sony2 from './images/SonyPS/sony2.jpg';
+//import sony3 from './images/SonyPS/sony-3.jpg';
+// const Apidata= [{"id":1,"title":"amd Ryzen 9 3900XT 3.8 GHz Upto 4.7 GHz AM4 Socket 12 Cores 24 Threads Desktop Processor  (Silver)","description":"From content creation to gaming, the AMD Ryzen 9 3900XT desktop processor is designed to support your hustle. With Max Boost clock speed of up to 4.7 GHz and 70 MB of cache memory, it offers efficient and powerful performance. Its AMD Ryzen Ready Socket AM4 computing platform is compatible with both AMD X570 and B550 chipsets to ensure convenient and powerful usage.","price":45889.0,"originalPrice":55990.0,"quantity":15,"discountPercent":18,"createdDate":"2021-05-24T05:06:56","productImage":[{"id":1,"imageUrl":"https://s3.ap-south-1.amazonaws.com/mycsyam.image/amd-ryzen-9-3900xt-original-imafyy4c94krgbxm.jpeg"},{"id":2,"imageUrl":"https://s3.ap-south-1.amazonaws.com/mycsyam.image/amd-ryzen-9-3900xt-original-imafyy4cuhupvurs.jpeg"},{"id":3,"imageUrl":"https://s3.ap-south-1.amazonaws.com/mycsyam.image/amd-ryzen-9-3900xt-original-imafyy4cye6zsbfh.jpeg"}],"rating":6,"expressDelivery":true},{"id":2,"title":"ZEBRONICS ZEB-TRANSFORMER-M Wired Optical Gaming Mouse (USB 3.0, Black)","description":"Zeb-Transformer-M is a premium gaming mouse that comes with 6 buttons. It has a high precision optical sensor with a dedicated DPI switch that can toggle between 1000/1600/ 2400/ 3200 DPI. The mouse has 7 breathable LED modes, a sturdy 1.8-meter braided cable, and comes with a high-quality USB connector.","price":444.0,"originalPrice":549.0,"quantity":7,"discountPercent":19,"createdDate":"2021-05-24T05:09:30","productImage":[{"id":4,"imageUrl":"https://s3.ap-south-1.amazonaws.com/mycsyam.image/zeb-transformer-m-zebronics-original-imafxrugfftphbkk.jpeg"},{"id":5,"imageUrl":"https://s3.ap-south-1.amazonaws.com/mycsyam.image/zeb-transformer-m-zebronics-original-imafxrugh8yqdryh.jpeg"},{"id":6,"imageUrl":"https://s3.ap-south-1.amazonaws.com/mycsyam.image/zeb-transformer-m-zebronics-original-imafxrugue4nkmrb.jpeg"}],"rating":4,"expressDelivery":true},{"id":3,"title":"HP Pavilion Gaming Ryzen 5 Quad Core 3550H - (8 GB/1 TB HDD/Windows 10 Home/4 GB Graphics/NVIDIA GeForce GTX 1650)","description":"If you’re looking for a sleek yet powerful laptop that is perfect for, both, work and play, then you must buy the HP Pavilion Gaming 15 Laptop. Whether it's playing games or multitasking between applications, this laptop offers a smooth and lag-free performance as it features an AMD Ryzen 5-3550H processor and 8 GB of DDR4 RAM. Enjoy an immersive viewing experience, thanks to the 39.62-cm (15.6) Full HD display and dual speakers as well as B&O Audio.","price":49990.0,"originalPrice":56144.0,"quantity":11,"discountPercent":10,"createdDate":"2021-05-24T05:16:18","productImage":[{"id":7,"imageUrl":"https://s3.ap-south-1.amazonaws.com/mycsyam.image/hp-na-gaming-laptop-original-imaftpes7f3xwaus.jpeg"},{"id":8,"imageUrl":"https://s3.ap-south-1.amazonaws.com/mycsyam.image/hp-na-gaming-laptop-original-imaftpesacqphdch.jpeg"},{"id":9,"imageUrl":"https://s3.ap-south-1.amazonaws.com/mycsyam.image/hp-na-gaming-laptop-original-imaftpesbvfxgw9t.jpeg"}],"rating":3,"expressDelivery":false},{"id":4,"title":"Sony PS5 DualSense Wireless Controller  (White, For PS5)","description":"DualSense, Wireless Controller, Instruction Manual, Bluetooth compatible with all devices","price":5990.0,"originalPrice":5990.0,"quantity":4,"discountPercent":0,"createdDate":"2021-05-24T05:23:46","productImage":[{"id":10,"imageUrl":"https://s3.ap-south-1.amazonaws.com/mycsyam.image/sony-dualsense-original-imafz66q8kv9nchz.jpeg"},{"id":11,"imageUrl":"https://s3.ap-south-1.amazonaws.com/mycsyam.image/sony-dualsense-original-imafz66qyhe75rxm.jpeg"}],"rating":3,"expressDelivery":false}]
+const Apidata=[
+  {
+    "id": 1,
+    "title": "amd Ryzen 9 3900XT 3.8 GHz Upto 4.7 GHz AM4 Socket 12 Cores 24 Threads Desktop Processor  (Silver)",
+    "description": "From content creation to gaming, the AMD Ryzen 9 3900XT desktop processor is designed to support your hustle. With Max Boost clock speed of up to 4.7 GHz and 70 MB of cache memory, it offers efficient and powerful performance. Its AMD Ryzen Ready Socket AM4 computing platform is compatible with both AMD X570 and B550 chipsets to ensure convenient and powerful usage.",
+    "price": 45889,
+    "originalPrice": 55990,
+    "quantity": 15,
+    "discountPercent": 18,
+    "createdDate": "2021-05-24T05:06:56",
+    "productImage": [
+      {
+        "id": 1,
+        "imageUrl": amd1
+      },
+      {
+        "id": 2,
+        "imageUrl": amd2
+      },
+      {
+        "id": 3,
+        "imageUrl": amd3
+      }
+    ],
+    "rating": 6,
+    "expressDelivery": true
+  },
+  {
+    "id": 2,
+    "title": "ZEBRONICS ZEB-TRANSFORMER-M Wired Optical Gaming Mouse (USB 3.0, Black)",
+    "description": "Zeb-Transformer-M is a premium gaming mouse that comes with 6 buttons. It has a high precision optical sensor with a dedicated DPI switch that can toggle between 1000/1600/ 2400/ 3200 DPI. The mouse has 7 breathable LED modes, a sturdy 1.8-meter braided cable, and comes with a high-quality USB connector.",
+    "price": 444,
+    "originalPrice": 549,
+    "quantity": 7,
+    "discountPercent": 19,
+    "createdDate": "2021-05-24T05:09:30",
+    "productImage": [
+      {
+        "id": 4,
+        "imageUrl": zeb1
+      },
+      {
+        "id": 5,
+        "imageUrl": zeb2
+      },
+      {
+        "id": 6,
+        "imageUrl": zeb3
+      }
+    ],
+    "rating": 4,
+    "expressDelivery": true
+  },
+  {
+    "id": 3,
+    "title": "HP Pavilion Gaming Ryzen 5 Quad Core 3550H - (8 GB/1 TB HDD/Windows 10 Home/4 GB Graphics/NVIDIA GeForce GTX 1650)",
+    "description": "If you’re looking for a sleek yet powerful laptop that is perfect for, both, work and play, then you must buy the HP Pavilion Gaming 15 Laptop. Whether it's playing games or multitasking between applications, this laptop offers a smooth and lag-free performance as it features an AMD Ryzen 5-3550H processor and 8 GB of DDR4 RAM. Enjoy an immersive viewing experience, thanks to the 39.62-cm (15.6) Full HD display and dual speakers as well as B&O Audio.",
+    "price": 49990,
+    "originalPrice": 56144,
+    "quantity": 11,
+    "discountPercent": 10,
+    "createdDate": "2021-05-24T05:16:18",
+    "productImage": [
+      {
+        "id": 7,
+        "imageUrl": hp1
+      },
+      {
+        "id": 8,
+        "imageUrl": hp2
+      },
+      {
+        "id": 9,
+        "imageUrl": hp3
+      }
+    ],
+    "rating": 3,
+    "expressDelivery": false
+  },
+  {
+    "id": 4,
+    "title": "Sony PS5 DualSense Wireless Controller  (White, For PS5)",
+    "description": "DualSense, Wireless Controller, Instruction Manual, Bluetooth compatible with all devices",
+    "price": 5990,
+    "originalPrice": 5990,
+    "quantity": 4,
+    "discountPercent": 0,
+    "createdDate": "2021-05-24T05:23:46",
+    "productImage": [
+      {
+        "id": 10,
+        "imageUrl": sony1
+      },
+      {
+        "id": 11,
+        "imageUrl": sony2
+      }
+    ],
+    "rating": 3,
+    "expressDelivery": false
+  }
+]
 
 class App extends React.Component{
   constructor(props){
@@ -32,10 +146,11 @@ class App extends React.Component{
   }
 
   componentDidMount(){
-    const corsurl = 'https://cors-anywhere.herokuapp.com';
-    axios.get(corsurl+Api).then((res)=>{
-      this.setState({isLoading:false,cartData:res.data})
-    });
+    // const corsurl = 'https://cors-anywhere.herokuapp.com';
+    // axios.get(corsurl+Api).then((res)=>{
+    //   this.setState({isLoading:false,cartData:res.data})
+    // });
+    this.setState({isLoading:false,cartData:Apidata})
   }
 
   searchFunction(e){
